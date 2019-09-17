9cecbc3bae180fa105719fade958491d6916d3e8
Veda <veda.balte@sitechhs.com>
2019-09-16 21:01:32 -0400

    Update quiz3.js

--- quiz3.js
+++ quiz3.js
@@ -1,4 +1,4 @@
-RaphBill = [140, 45, 270];
+//RaphBill = [140, 45, 270];
 
 // const map1 = RaphBill.map(x => x * 0.20); //20 percent when les than 50
 
@@ -21,24 +21,45 @@ let Tips = [0.15,0.2,0.10]; */
 
 /////QUIZ THREE 
 
+function printArray(someArray, message) {
+    console.log(`${someArray} ${message}`);
+}
+//array for bills
+const bill = [140, 45, 270]; 
+printArray(bill, 'this array shows the bill without tip');
 
+//this function gives you the total tip for a bill
 function tipCalculator(bill) {
-    let percentage; 
+    let percentage;
+    if (bill < 0) {
+        throw new Error('Bill must be a positive number!!!');
+    }
     if (bill < 50) {
         percentage = 0.2;
-    } else if(bill >= 50 && bill < 200.) {
+    } else if (bill >= 50 && bill <= 200.) {
         percentage = 0.15;
-
     } else {
         percentage = 0.1;
     }
     return percentage * bill;
-}
+} 
 
-const bill = [140, 45, 270]; 
-const billTip = [tipCalculator(bill[0]), tipCalculator(bill[1]), tipCalculator(bill[2])] ;
+//bill tip 
+const billTip = bill.map(val => tipCalculator(val));
+printArray(billTip, `this array shows the tip`); 
 
-const billTotal = [ tipCalculator(bill[0]) + bill[0], tipCalculator(bill[1]) + bill[1], tipCalculator(bill[2]) + bill[2]]; 
+//bill total
+const billTotal = bill.map(val => {
+    const index = bill.indexOf(val);
+    return val + billTip[index]; 
+}); 
+printArray(billTotal, `this array shows the total bill with tip`); 
+
+//over 100
+var copyArray = billTotal.filter(value => value >= 100);
+printArray(copyArray, `this array shows the total bill value over $100`); 
+
+
 
 
 
